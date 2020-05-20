45ac142cb39134f52a2a191d02a862a749e7458b
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2020-05-20 16:50:03 +0700

    Seperate TextArea as a part of code

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,7 +8,7 @@ import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame{
 	
-	private JTextArea txtArea;
+	private TextPanel txtPanel;
 	private JButton btn;
 	
 	public MainFrame() {
@@ -16,16 +16,16 @@ public class MainFrame extends JFrame{
 		
 		setLayout(new BorderLayout());
 		
-		txtArea = new JTextArea();
+		txtPanel = new TextPanel();
 		btn = new JButton("Click Me!");
 		
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				txtArea.append("Hello\n");
+				txtPanel.appendText("Hello\n");
 			}
 		});
 		
-		add(txtArea, BorderLayout.CENTER);
+		add(txtPanel, BorderLayout.CENTER);
 		add(btn, BorderLayout.SOUTH);
 		
 		setSize(600, 500);
---
+++ src/TextPanel.java
@@ -0,0 +1,23 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class TextPanel extends JPanel {
+	private JTextArea txtArea;
+	
+	public TextPanel() {
+		txtArea = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		
+//		Add TextArea with Scrollbar
+		add(new JScrollPane(txtArea), BorderLayout.CENTER);
+	}
+	
+	public void appendText(String txt) {
+		txtArea.append(txt);
+	}
+}
