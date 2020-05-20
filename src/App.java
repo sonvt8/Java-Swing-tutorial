36ec3c82cc66a78d72387db7defe8876b8c489f0
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2020-05-20 16:30:54 +0700

    Add Components to BorderLayout

--- src/App.java
+++ src/App.java
@@ -6,10 +6,7 @@ public class App {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				JFrame frame = new JFrame("Hello World");
-				frame.setSize(600, 500);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
+				new MainFrame();
 			}
 		});
 	}
