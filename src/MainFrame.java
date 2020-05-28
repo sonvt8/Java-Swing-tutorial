d6a1b85a722a17f11beed01bbbcb4552e6fd3d46
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2020-05-28 17:03:25 +0700

    Menus and Menu Item

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame{
@@ -20,6 +23,8 @@ public class MainFrame extends JFrame{
 		toolbar = new Toolbar();
 		formPanel = new FormPanel();
 		txtPanel = new TextPanel();
+		
+		setJMenuBar(createMenuBar());
 		
 		toolbar.setStringListener(new StringListener() {
 			@Override
@@ -46,5 +51,30 @@ public class MainFrame extends JFrame{
 		setSize(600, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	
+	private JMenuBar createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem exportDataItem = new JMenuItem("Export Data...");
+		JMenuItem importDataItem = new JMenuItem("Import Data...");
+		JMenuItem exitItem = new JMenuItem("Exit");
+		
+		fileMenu.add(exportDataItem);
+		fileMenu.add(importDataItem);
+		fileMenu.addSeparator();
+		fileMenu.add(exitItem);
+		
+		JMenu windowMenu = new JMenu("Window");
+		JMenu showMenu = new JMenu("Show");
+		JMenuItem showFormItem = new JMenuItem("Person Form");
+		showMenu.add(showFormItem);
+		windowMenu.add(showMenu);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(windowMenu);
+		
+		return menuBar;
 	}
 }
