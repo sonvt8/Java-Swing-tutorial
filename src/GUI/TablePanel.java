45cb8754f43d2007efd8cd781f996a903837ddb1
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2020-05-31 10:28:52 +0700

    Popup message when right clicking on the row of table

--- src/GUI/TablePanel.java
+++ src/GUI/TablePanel.java
@@ -1,9 +1,13 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -13,10 +17,25 @@ public class TablePanel extends JPanel {
 	
 	private JTable table;
 	private PersonTableModel tableModel;
+	private JPopupMenu popup;
 	
 	public TablePanel() {
 		tableModel = new PersonTableModel();
 		table = new JTable(tableModel);
+		popup = new JPopupMenu();
+		
+		JMenuItem removeItem = new JMenuItem("Delete Row");
+		popup.add(removeItem);
+		
+		table.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if(e.getButton() == MouseEvent.BUTTON3) {
+					//When right click on the row of table, the pop up will be showed up
+					popup.show(table, e.getX(), e.getY());
+				}
+			}
+			
+		});
 		
 		setLayout(new BorderLayout());
 		
