a049b7118caa3f3bef25775c81013c5d450b93c1
Son Vu <ly.thl2505@sinhvien.hoasen.edu.vn>
2020-06-24 17:33:16 +0700

    Listener Mouse Click and System Print node on JTree

--- src/GUI/MessagePanel.java
+++ src/GUI/MessagePanel.java
@@ -5,13 +5,28 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
 public class MessagePanel extends JPanel {
 	private JTree serverTree;
 	
 	public MessagePanel() {
 		serverTree = new JTree(creatTree());
+		
+		serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		
+		serverTree.addTreeSelectionListener(new TreeSelectionListener() {
+			public void valueChanged(TreeSelectionEvent e) {
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) serverTree.getLastSelectedPathComponent();
+				
+				Object userObject = node.getUserObject();
+				
+				System.out.println(userObject);
+			}
+		});
 		
 		setLayout(new BorderLayout());
 		
